51a27622e58fe9218a2b87cca7997811e6aea1a7
C-Buckingham <supergluedchain@gmail.com>
2015-12-27 17:04:41 +0000

    Updated Some Files

---
+++ comparson_code/python_Comparison_Code.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+"""
+Spyder Editor
+
+This temporary script file is located here:
+/home/chris/.spyder2/.temp.py
+"""
+
+import numpy as np
+import cv2
+
+image = cv2.imread('/home/chris/catkin_ws/src/comparson_code/71P67LGRY_large.jpg')
