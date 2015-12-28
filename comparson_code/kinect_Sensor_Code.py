6372663ef9f93de002fce1629b83aca39350f3ed
C-Buckingham <supergluedchain@gmail.com>
2015-12-28 13:07:23 +0000

    Messing with Tkiner for key press detection

--- comparson_code/kinect_Sensor_Code.py
+++ comparson_code/kinect_Sensor_Code.py
@@ -3,11 +3,17 @@
 import rospy
 import cv2
 import numpy
+import Tkinter as tk
 
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
-#
-class image_get:
+
+import Tkinter as tk
+
+def onKeyPress(event):
+    text.insert('end', 'You pressed %s\n' % (event.char, ))
+
+class video_get:
 
     def __init__(self):
         cv2.namedWindow("Image window", 1)
@@ -17,18 +23,40 @@ class image_get:
         self.image_sub = rospy.Subscriber(
             "/camera/rgb/image_color",
             Image,
-            callback=self.iamge_callback
+            callback=self.image_callback
         )
+        
     
-    def iamge_callback(self, img):
+    def image_callback(self, img):
         try:
             cv_image = self.bridge.imgmsg_to_cv2(img, "bgr8")
         except CvBridgeError, e:
             print e
             
-        cv2.imshow("Image window", cv_image)
+        print numpy.mean(cv_image[:, :, 0])
+        print numpy.mean(cv_image[:, :, 1])
+        print numpy.mean(cv_image[:, :, 2])
+        
+        print '===='
+        blue_mean = numpy.mean(cv_image[:, :, 0])
+        green_mean = numpy.mean(cv_image[:, :, 1])
+        red_mean = numpy.mean(cv_image[:, :, 2])
+        
+        bgr_mean = (blue_mean + green_mean + red_mean) / 3      
+        
+        
+        print bgr_mean
+        print '===='
+        
+        cv2.imshow("Image window", cv_image)      
 
-image_get()
+video_get()
 rospy.init_node('image_get', anonymous=True)
 rospy.spin()
+root = tk.Tk()
+root.geometry('300x200')
+text = tk.Text(root, background='black', foreground='white', font=('Comic Sans MS', 12))
+text.pack()
+root.bind('<KeyPress>', onKeyPress)
+root.mainloop()  
 cv2.destroyAllWindows()
--- comparson_code/python_Comparison_Code.py
+++ comparson_code/python_Comparison_Code.py
@@ -8,5 +8,8 @@ This temporary script file is located here:
 
 import numpy as np
 import cv2
+from matplotlib import pyplot as plt
 
 image = cv2.imread('/home/chris/catkin_ws/src/comparson_code/71P67LGRY_large.jpg')
+plt.hist(image.ravel(),256,[0,256]); 
+plt.show()
