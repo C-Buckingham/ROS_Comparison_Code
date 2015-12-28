6bab373a8b78b54e8e3017715b39b5ea5cd69044
C-Buckingham <supergluedchain@gmail.com>
2015-12-28 14:46:05 +0000

    Different T-Shirt detection in real time working on RGB colour space only

--- comparson_code/kinect_Sensor_Code.py
+++ comparson_code/kinect_Sensor_Code.py
@@ -8,10 +8,15 @@ import Tkinter as tk
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 
-import Tkinter as tk
+#import Tkinter as tk
+#
+#def onKeyPress(event):
+#    text.insert('end', 'You pressed %s\n' % (event.char, ))
 
-def onKeyPress(event):
-    text.insert('end', 'You pressed %s\n' % (event.char, ))
+base_image = cv2.imread("/home/chris/Pictures/Screenshot from 2015-12-28 14:25:55.png")
+blue_base_image_hist = cv2.calcHist([base_image],[0],None,[256],[0,256])
+green_base_image_hist = cv2.calcHist([base_image],[1],None,[256],[0,256])
+red_base_image_hist = cv2.calcHist([base_image],[2],None,[256],[0,256])
 
 class video_get:
 
@@ -48,15 +53,35 @@ class video_get:
         print bgr_mean
         print '===='
         
+        blue_video_hist = cv2.calcHist([cv_image],[0],None,[256],[0,256])
+        green_video_hist = cv2.calcHist([cv_image],[1],None,[256],[0,256])
+        red_video_hist = cv2.calcHist([cv_image],[2],None,[256],[0,256])
+        
+        blue_histogram_comparison_result = cv2.compareHist(blue_base_image_hist, blue_video_hist, cv2.cv.CV_COMP_CORREL)        
+        green_histogram_comparison_result = cv2.compareHist(green_base_image_hist, green_video_hist, cv2.cv.CV_COMP_CORREL)   
+        red_histogram_comparison_result = cv2.compareHist(red_base_image_hist, red_video_hist, cv2.cv.CV_COMP_CORREL)   
+        
+        avg_correlation = (blue_histogram_comparison_result+green_histogram_comparison_result+red_histogram_comparison_result)/3        
+        
+        print avg_correlation
+        print '===='
+
+        if avg_correlation < 0.85:
+            print 'Different T-Shirt'
+        else:
+            print 'Same T-Shirt'                
+        
         cv2.imshow("Image window", cv_image)      
 
 video_get()
 rospy.init_node('image_get', anonymous=True)
 rospy.spin()
-root = tk.Tk()
-root.geometry('300x200')
-text = tk.Text(root, background='black', foreground='white', font=('Comic Sans MS', 12))
-text.pack()
-root.bind('<KeyPress>', onKeyPress)
-root.mainloop()  
+
+#root = tk.Tk()
+#root.geometry('300x200')
+#text = tk.Text(root, background='black', foreground='white', font=('Comic Sans MS', 12))
+#text.pack()
+#root.bind('<KeyPress>', onKeyPress)
+#root.mainloop()  
+
 cv2.destroyAllWindows()
