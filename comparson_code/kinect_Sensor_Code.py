c15b9fa5da4af1571df7b0a886894ae93725f1d5
C-Buckingham <supergluedchain@gmail.com>
2015-12-29 14:38:45 +0000

    Program now uses the screenshot to compare against the live feed.

--- comparson_code/kinect_Sensor_Code.py
+++ comparson_code/kinect_Sensor_Code.py
@@ -9,9 +9,7 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 
 no_input = True
-
-base_image = cv2.imread("/home/chris/catkin_ws/src/comparson_code/Base_Image.png")
-hsv_base_image = cv2.cvtColor(base_image, cv2.COLOR_BGR2HSV)
+no_base_image = True
 
 class video_get:
 
@@ -25,64 +23,74 @@ class video_get:
             Image,
             callback=self.image_callback
         )
-        
-    
+            
     def image_callback(self, img):
-        global no_input
         try:
             video_image = self.bridge.imgmsg_to_cv2(img, "bgr8")
         except CvBridgeError, e:
             print e
+        global no_input
+        global no_base_image
+        if no_base_image:
+            print 'No image to compare against.'
+            if no_input == False:
+                print "Screen Grab"
+                screen_grab = video_image
+                cv2.imwrite("Base.jpg", screen_grab)            
+                no_input = True  
+                no_base_image = False   
+        else:            
+            base_image = cv2.imread("/home/chris/catkin_ws/src/comparson_code/Base.jpg")
+            hsv_base_image = cv2.cvtColor(base_image, cv2.COLOR_BGR2HSV)             
             
-        hsv_video_image = cv2.cvtColor(video_image, cv2.COLOR_BGR2HSV)
-        
-        if no_input == False:
-            print "Screen Grab"
-            screen_grab = video_image
-            cv2.imwrite("Base.jpg", screen_grab)            
-            no_input = True    
-
-        bgr_comparison_result = []
-        hsv_comparison_result = []
-        
-        for x in range (0, 3):
-            bgr_video_image_hist = cv2.calcHist([video_image], [x], None, [256], [0,256])
-            bgr_base_image_hist = cv2.calcHist([base_image], [x], None, [256], [0,256])
-            bgr_comparison_result.append(cv2.compareHist(bgr_video_image_hist, bgr_base_image_hist, cv2.cv.CV_COMP_CORREL))
-        
-        for x in range (0, 2):
-            hsv_video_image_hist = cv2.calcHist([hsv_video_image],[x],None,[256],[0,256])
-            hsv_base_image_hist = cv2.calcHist([hsv_base_image],[x],None,[256],[0,256])                
-            hsv_comparison_result.append(cv2.compareHist(hsv_video_image_hist, hsv_base_image_hist, cv2.cv.CV_COMP_CORREL))
+            hsv_video_image = cv2.cvtColor(video_image, cv2.COLOR_BGR2HSV)
             
-        bgr_avg_correlation = numpy.mean(bgr_comparison_result)
-        hsv_avg_correlation = numpy.mean(hsv_comparison_result)
-        
-#        print ('bgr: ', bgr_avg_correlation)
-#        print '===='
-#        print ('hsv: ', hsv_avg_correlation)
-        
-        if (bgr_avg_correlation+hsv_avg_correlation)/2 < 0.85:
-            print 'Different'
-        else:
-            print 'Same' 
-               
-        print '===='        
-        
-        cv2.imshow("Image window", video_image)
+            if no_input == False:
+                print "Screen Grab"
+                screen_grab = video_image
+                cv2.imwrite("Base.jpg", screen_grab)            
+                no_input = True    
+    
+            bgr_comparison_result = []
+            hsv_comparison_result = []
+            
+            for x in range (0, 3):
+                bgr_video_image_hist = cv2.calcHist([video_image], [x], None, [256], [0,256])
+                bgr_base_image_hist = cv2.calcHist([base_image], [x], None, [256], [0,256])
+                bgr_comparison_result.append(cv2.compareHist(bgr_video_image_hist, bgr_base_image_hist, cv2.cv.CV_COMP_CORREL))
+            
+            for x in range (0, 2):
+                hsv_video_image_hist = cv2.calcHist([hsv_video_image],[x],None,[256],[0,256])
+                hsv_base_image_hist = cv2.calcHist([hsv_base_image],[x],None,[256],[0,256])                
+                hsv_comparison_result.append(cv2.compareHist(hsv_video_image_hist, hsv_base_image_hist, cv2.cv.CV_COMP_CORREL))
+                
+            bgr_avg_correlation = numpy.mean(bgr_comparison_result)
+            hsv_avg_correlation = numpy.mean(hsv_comparison_result)
+            
+    #        print ('bgr: ', bgr_avg_correlation)
+    #        print '===='
+    #        print ('hsv: ', hsv_avg_correlation)
+            
+            if (bgr_avg_correlation+hsv_avg_correlation)/2 < 0.85:
+                print 'Different'
+            else:
+                print 'Same' 
+                   
+            print '===='        
+            
+            cv2.imshow("Image window", video_image)        
         
     def signal_user_input():
         global no_input
-        i = raw_input("hit enter to caputre base image")   # I have python 2.7, not 3.x
+        i = raw_input()
         no_input = False
-    #    cv2.VideoCapture.grab() 
+    
 
     threading.Thread(target = signal_user_input).start()
     
-while no_input:
-    no_input == True              
-    video_get()
-    rospy.init_node('image_get', anonymous=True)
-    rospy.spin()
+no_input == True              
+video_get()
+rospy.init_node('image_get', anonymous=True)
+rospy.spin()
     
 cv2.destroyAllWindows()
