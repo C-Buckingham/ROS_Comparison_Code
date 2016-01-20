6b5ea23d7cc607a3db7d7e7ecf0df51da80ebb67
C-Buckingham <chris7789@hotmail.co.uk>
2016-01-20 17:20:41 +0000

    Improved the ORB features, now does a comparison between picture and video

--- ORB_Test_Code.py
+++ ORB_Test_Code.py
@@ -3,7 +3,6 @@
 import rospy
 import numpy as np
 import cv2
-from matplotlib import pyplot as plt
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 
@@ -17,6 +16,54 @@ from cv_bridge import CvBridge, CvBridgeError
 #
 #img2 = cv2.drawKeypoints(img,kp,color=(0,255,0), flags=0)
 #plt.imshow(img2),plt.show()
+
+def draw_matches(img1, kp1, img2, kp2, matches, color=None): 
+    """Draws lines between matching keypoints of two images.  
+    Keypoints not in a matching pair are not drawn.
+    Places the images side by side in a new image and draws circles 
+    around each keypoint, with line segments connecting matching pairs.
+    You can tweak the r, thickness, and figsize values as needed.
+    Args:
+        img1: An openCV image ndarray in a grayscale or color format.
+        kp1: A list of cv2.KeyPoint objects for img1.
+        img2: An openCV image ndarray of the same format and with the same 
+        element type as img1.
+        kp2: A list of cv2.KeyPoint objects for img2.
+        matches: A list of DMatch objects whose trainIdx attribute refers to 
+        img1 keypoints and whose queryIdx attribute refers to img2 keypoints.
+        color: The color of the circles and connecting lines drawn on the images.  
+        A 3-tuple for color images, a scalar for grayscale images.  If None, these
+        values are randomly generated.  
+    """
+    # We're drawing them side by side.  Get dimensions accordingly.
+    # Handle both color and grayscale images.
+    if len(img1.shape) == 3:
+        new_shape = (max(img1.shape[0], img2.shape[0]), img1.shape[1]+img2.shape[1], img1.shape[2])
+    elif len(img1.shape) == 2:
+        new_shape = (max(img1.shape[0], img2.shape[0]), img1.shape[1]+img2.shape[1])
+    new_img = np.zeros(new_shape, type(img1.flat[0]))  
+    # Place images onto the new image.
+    new_img[0:img1.shape[0],0:img1.shape[1]] = img1
+    new_img[0:img2.shape[0],img1.shape[1]:img1.shape[1]+img2.shape[1]] = img2
+    
+    # Draw lines between matches.  Make sure to offset kp coords in second image appropriately.
+    r = 15
+    thickness = 2
+    if color:
+        c = color
+    for m in matches:
+        # Generate random color for RGB/BGR and grayscale images as needed.
+        if not color: 
+            c = np.random.randint(0,256,3) if len(img1.shape) == 3 else np.random.randint(0,256)
+        # So the keypoint locs are stored as a tuple of floats.  cv2.line(), like most other things,
+        # wants locs as a tuple of ints.
+        end1 = tuple(np.round(kp1[m.trainIdx].pt).astype(int))
+        end2 = tuple(np.round(kp2[m.queryIdx].pt).astype(int) + np.array([img1.shape[1], 0]))
+        cv2.line(new_img, end1, end2, c, thickness)
+        cv2.circle(new_img, end1, r, c, thickness)
+        cv2.circle(new_img, end2, r, c, thickness)
+    
+    return new_img
 
 class image_converter:
     
@@ -36,14 +83,24 @@ class image_converter:
             video_image = self.bridge.imgmsg_to_cv2(img, "bgr8")
         except CvBridgeError, e:
             print e
-            
+        base_image  = cv2.imread('/home/chris/catkin_ws/src/ROS_Comparison_Code/Base.jpg')    
+        
         orb = cv2.ORB()
 
-        kp = orb.detect(video_image,None)
+        kp1, des1 = orb.detectAndCompute(video_image,None)
+        kp2, des2 = orb.detectAndCompute(base_image, None)
+        
+        bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+        
+        matches = bf.match(des1,des2)
 
-        kp, des = orb.compute(video_image, kp)
+        matches = sorted(matches, key = lambda x:x.distance)
+        
+        video_image = draw_matches(video_image,kp1,base_image,kp2,matches[:10])
+        
+#        kp1, des = orb.compute(video_image, kp1)
 
-        video_image = cv2.drawKeypoints(video_image,kp,color=(0,255,0), flags=0)
+#        video_image = cv2.drawKeypoints(video_image,kp1,color=(0,255,0), base_image,kp2,color(0,255,0), flags=0)
         
         cv2.imshow("Image window", video_image)  
         
