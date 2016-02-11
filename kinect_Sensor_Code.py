e87dd9bb24156d4a7bdb356ba44fe6bf5cf1819a
C-Buckingham <SUPERgluedCHAIN@gmail.com>
2016-02-11 17:53:25 +0000

    Got the dictionary function working properly now.

--- kinect_Sensor_Code.py
+++ kinect_Sensor_Code.py
@@ -3,14 +3,12 @@
 import rospy
 import cv2
 import numpy as np
-import threading
 
 from sensor_msgs.msg import Image
 from upper_body_detector.msg import UpperBodyDetector
 from cv_bridge import CvBridge, CvBridgeError
 from message_filters import ApproximateTimeSynchronizer, Subscriber
 
-no_input = True
 no_base_image = True
 count = 0
 
@@ -37,118 +35,117 @@ class person_comparison:
         ts = ApproximateTimeSynchronizer([image_sub, person_sub], 10, 0.1)
         ts.registerCallback(self.image_callback)
 
+    def colour_Matching(base_image, video_image):
+#        print "Colour Matching"
+        hsv_base_image = cv2.cvtColor(base_image, cv2.COLOR_RGB2HSV)             
+                    
+        hsv_video_image = cv2.cvtColor(video_image, cv2.COLOR_BGR2HSV)
+                
+        bgr_comparison_result = []
+        hsv_comparison_result = []
+        
+        for x in range (0, 3):
+            bgr_video_image_hist = cv2.calcHist([video_image], [x], None, [256], [0,256])
+            bgr_base_image_hist = cv2.calcHist([base_image], [x], None, [256], [0,256])
+            bgr_comparison_result.append(cv2.compareHist(bgr_video_image_hist, bgr_base_image_hist, cv2.cv.CV_COMP_CORREL))
+            print bgr_comparison_result[x]
+            if (x < 2):
+                hsv_video_image_hist = cv2.calcHist([hsv_video_image],[x],None,[256],[0,256])
+                hsv_base_image_hist = cv2.calcHist([hsv_base_image],[x],None,[256],[0,256])                
+                hsv_comparison_result.append(cv2.compareHist(hsv_video_image_hist, hsv_base_image_hist, cv2.cv.CV_COMP_CORREL))
+            
+        bgr_avg_correlation = np.mean(bgr_comparison_result)
+        hsv_avg_correlation = np.mean(hsv_comparison_result)
+        
+        print ('bgr: ', bgr_avg_correlation)
+#        print '===='
+        print ('hsv: ', hsv_avg_correlation)
+        
+        if bgr_avg_correlation > 0.85 or hsv_avg_correlation > 0.85:
+            print 'Same'
+        else:
+            print 'Different' 
+               
+        print '==='
+    
+    def feature_Matching():
+        print "Feature Matching"
+
+        kp1, des1 = orb.detectAndCompute(video_image,None)
+        kp2, des2 = orb.detectAndCompute(base_image, None)
+        
+#                    bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+        bf = cv2.BFMatcher()
+        
+        match = bf.match(des1,des2)
+        matches = bf.knnMatch(des1, des2)                    
+        
+#                    Why I am using ORB: https://www.willowgarage.com/sites/default/files/orb_final.pdf
+        
+        good = []
+        for m,n in matches:
+            if m.distance < 0.75*n.distance:
+                good.append([m])
+        
+        match = sorted(match, key = lambda x:x.distance)
+        
+        video_image = cv2.drawKeypoints(video_image,kp1,color=(0,255,0), flags=0)
+        base_image = cv2.drawKeypoints(base_image,kp2,color=(0,255,0), flags=0)
+
+    global options
+    
+    options = {'C':colour_Matching, 'F':feature_Matching}
+
     def image_callback(self, img, person):
         
-        person_x = person.pos_x
-        person_y = person.pos_y
+        person_pos_x = person.pos_x
+        person_pos_y = person.pos_y
         person_height = person.height        
         person_width = person.width
+        
+        
+        def clamp(n, minn, maxn):
+            return max(min(maxn, n), minn)
         
         try:
             video_image = self.bridge.imgmsg_to_cv2(img, "bgr8")
         except CvBridgeError, e:
             print e
             
-        global no_input
         global screen_grab
         global no_base_image
         global count
         global choice
         
-        if (person_height):
-            max(person_height, 480)
-            max(person_width, 640)
-            max(person_x, 480)
-            max(person_y, 640)
-            
-            min(person_height, 0)
-            min(person_width, 0)
-            min(person_x, 0)
-            min(person_y, 0)
-                            
-        print "Press Enter to capture base image"
+        if (person_height):               
         
-        if no_input == False:
-            print "No Person"
-            if (person_height):
-                count = count + 1
-                print count                    
-                
-            if (count > 20):
-                screen_grab = video_image[person_y[0]:(person_y[0]+person_width[0])*2, person_x[0]:person_x[0]+person_height[0]]          
-                no_input = True  
-                no_base_image = False
+            person_h = min(person_height[0], 480)
+            person_w = min(person_width[0], 640)
+            person_x = min(person_pos_x[0], 480)
+            person_y = min(person_pos_y[0], 640)
             
-        elif (no_base_image and no_input):
+            person_h = max(person_height[0], 0)
+            person_w = max(person_width[0], 0)
+            person_x = max(person_pos_x[0], 0)
+            person_y = max(person_pos_y[0], 0)
+
+            count = count + 1
+            print count                    
+
+        if (count == 20):
+            screen_grab = video_image[person_y:(person_y+person_w)*2, person_x:person_x+person_h]          
+            no_base_image = False
+            count = 0 
+            
+        if (no_base_image == False):
+            
             base_image = screen_grab
-            if (person_y):
-                video_image = video_image[person_y[0]:(person_y[0]+person_width[0])*2, person_x[0]:person_x[0]+person_height[0]]
-                if (choice == 'F'):
-                    orb = cv2.ORB()
-    
-                    kp1, des1 = orb.detectAndCompute(video_image,None)
-                    kp2, des2 = orb.detectAndCompute(base_image, None)
-                    
-#                    bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-                    bf = cv2.BFMatcher()
-                    
-                    match = bf.match(des1,des2)
-                    matches = bf.knnMatch(des1, des2)                    
-                    
-#                    Why I am using ORB: https://www.willowgarage.com/sites/default/files/orb_final.pdf
-                    
-                    good = []
-                    for m,n in matches:
-                        if m.distance < 0.75*n.distance:
-                            good.append([m])
-                    
-            
-                    match = sorted(match, key = lambda x:x.distance)
-                    
-                    video_image = cv2.drawKeypoints(video_image,kp1,color=(0,255,0), flags=0)
-                    base_image = cv2.drawKeypoints(base_image,kp2,color=(0,255,0), flags=0)
-                    
-                else:
-                    hsv_base_image = cv2.cvtColor(base_image, cv2.COLOR_RGB2HSV)             
-                    
-                    hsv_video_image = cv2.cvtColor(video_image, cv2.COLOR_BGR2HSV)
-                            
-                    bgr_comparison_result = []
-                    hsv_comparison_result = []
-                    
-                    for x in range (0, 3):
-                        bgr_video_image_hist = cv2.calcHist([video_image], [x], None, [256], [0,256])
-                        bgr_base_image_hist = cv2.calcHist([base_image], [x], None, [256], [0,256])
-                        bgr_comparison_result.append(cv2.compareHist(bgr_video_image_hist, bgr_base_image_hist, cv2.cv.CV_COMP_CORREL))
-                        if (x < 2):
-                            hsv_video_image_hist = cv2.calcHist([hsv_video_image],[x],None,[256],[0,256])
-                            hsv_base_image_hist = cv2.calcHist([hsv_base_image],[x],None,[256],[0,256])                
-                            hsv_comparison_result.append(cv2.compareHist(hsv_video_image_hist, hsv_base_image_hist, cv2.cv.CV_COMP_CORREL))
-                        
-                    bgr_avg_correlation = np.mean(bgr_comparison_result)
-                    hsv_avg_correlation = np.mean(hsv_comparison_result)
-                    
-                    print ('bgr: ', bgr_avg_correlation)
-            #        print '===='
-                    print ('hsv: ', hsv_avg_correlation)
-                    
-                    if bgr_avg_correlation > 0.85 or hsv_avg_correlation > 0.85:
-                        print 'Same'
-                    else:
-                        print 'Different' 
-                           
-                    print '==='
+            if (person_pos_y):
+                video_image = video_image[person_y:(person_y+person_w)*2, person_x:person_x+person_h]
+                options[choice](base_image, video_image)
             
             cv2.imshow("Live Image", video_image)
             cv2.imshow("Base Image", base_image)
-        
-    def signal_user_input():
-        global no_input
-        i = raw_input()
-        no_input = False
-    
-
-    threading.Thread(target = signal_user_input).start()
 
 person_comparison()
 rospy.init_node('person_comparison', anonymous=True)
